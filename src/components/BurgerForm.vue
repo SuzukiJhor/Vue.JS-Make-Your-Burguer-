845f787d334190bf87f492a6ccf718d43d51947e
Jhordan S <jhordan.kendy@outlook.com>
2023-10-17 15:26:35 -0300

    Feat: adicionando formulário a home.

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <p>Componente de mensagem</p>
+    <div>
+        <form id="burger-form">
+            <div class="input-container">
+                <label for="name">Name Client</label>
+                <input type="text" id="name" name="name" v-model="name" placeholder="Digite Seu nome">
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "BurgerForm"
+}
+</script>
+
+<Style scoped>
+
+    </Style>
