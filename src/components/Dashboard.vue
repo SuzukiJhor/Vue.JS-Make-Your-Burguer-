b7fb997b92582c1e0aa245f666b5b6280ae590d7
Jhordan S <jhordan.kendy@outlook.com>
2023-10-18 19:59:14 -0300

    Feat: ajustando status dos pedidos na tela de pedidos.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -23,7 +23,7 @@
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
+          <select name="status" class="status" @change="updateBurger($event, burger.id)">
             <option>
               Selecione
             </option>
@@ -31,7 +31,7 @@
               {{ s.tipo }}
             </option>
           </select>
-          <button class="delete-btn">Cancelar</button>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
       </div>
     </div>
@@ -65,6 +65,33 @@ export default {
       const data = await req.json();
 
       this.status = data;
+    },
+
+    async deleteBurger(id) {
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: 'DELETE'
+      })
+
+      const res = await req.json();
+
+      //msg
+
+      this.getPedidos()
+    },
+
+    async updateBurger(event, id) {
+      const option = event.target.value;
+      const dataJson = JSON.stringify({ status: option });
+
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: dataJson
+      })
+
+      const res = await req.json();
+
+      console.log(res);
     }
   },
   mounted() {
