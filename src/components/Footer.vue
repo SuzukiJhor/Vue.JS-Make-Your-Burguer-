5e6102ca1e1cf7c3c16412b847087b30dce870fe
Jhordan S <jhordan.kendy@outlook.com>
2023-10-18 15:35:16 -0300

    Feat: tabela na tela de pedidos.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,13 +12,14 @@ export default {
   
 <style scoped>
 #footer {
-    height: 200px;
+    height: 100px;
     background-color: #222;
     border-top: 4px solid #111;
     color: #fcba03;
     display: flex;
     align-items: center;
     justify-content: center;
+    position: fixed;
     bottom: 0;
     width: 100%;
 }
